df5ac677a2fab41185986fd2104e3ed9894f8515
Noah Segal <n.h.segal@gmail.com>
2017-05-21 12:23:50 -0400

    Changed script reference
    From Dropbox public to a local file

--- sketch3.js
+++ sketch3.js
@@ -1,240 +1,317 @@
-var nodeNumSlider;
+var redtrains = []; 
+var bluetrains = [];
+var trainLength = 14;
 
-var pChoice0;
-var pChoice1;
-var pChoice2;
-var pChoice3;
-var pChoice4;
-var pChoice5;
+var leftRed, leftRedSerial, leftRedClock, rightRedSerial, rightRedClock ,leftBlueSerial, leftBlueClock, rightBlueSerial, rightBlueClock;
 
-var nodes = [];
-var potential = 1;
-var nodeNum = 9;
-var initialAction = 0;
-var initialU = 0;
-var initialK = 0;
-var scaleFactor;
-var most;
+var m, n, leftPhoto, rightPhoto; //Meteors
+var leftYes = false; //is there a meteor in the left photo?
+var rightYes = false;
+
+var timer = 0;
+var counter = 0;  //counts photos taken
+var metcounter = 0; //counts pairs of meteors made
 
 function setup() {
-  createCanvas(800, 400);
- 
-  /*pChoice0 = createButton('free particle', 0);
-  pChoice1 = createButton('linear gradient upward', 1);
-  pChoice2 = createButton('linear gradient leftward', 2);
-  pChoice3 = createButton('energy step', 3);
-  pChoice4 = createButton('inverse well', 4);
-  pChoice5 = createButton('simple harmonic', 5);
+    createCanvas(1000, 400);
+    textFont("Courier"); 
+    textAlign(LEFT);
+    textSize(16);
+    strokeWeight(0.1);
 
-  pChoice0.mousePressed(function() {potential = 0;});
-  pChoice1.mousePressed(function() {potential = 1;});
-  pChoice2.mousePressed(function() {potential = 2;});
-  pChoice3.mousePressed(function() {potential = 3;});
-  pChoice4.mousePressed(function() {potential = 4;});
-  pChoice5.mousePressed(function() {potential = 5;});
+  // Train(color c_, float xpos_, float ypos_, float xspeed_, int serial_, float offset_, int direction_) 
 
-  pChoice0.position(810, 10);
-  pChoice1.position(810, 35);
-  pChoice2.position(810, 60);
-  pChoice3.position(810, 85);
-  pChoice4.position(810, 110);
-  pChoice5.position(810, 135);
-  */
-  nodeNumSlider = createSlider(3,17,11);
-  nodeNumSlider.position(10,450);
-  nodeNumSlider.size(300);  
-
-  stroke(100);
-  noStroke();
-  for (var i = 0; i<nodeNumSlider.value(); i++) {
-    nodes.push(new Node(map(i, 0, nodeNumSlider.value()-1, 30, width-80), height/2));
-  }
+    for (var i = 0; i < trainLength; i++ ) {
+        redtrains.push(new Train(color(250, 120, 120), width/2 - 4 - 70*i, 60, -1, i+1, i*.02, -1));
+    }
+    for (var i = 0; i < trainLength; i++ ) {
+        bluetrains.push(new Train(color(120, 120, 250), width/2 + 4 + 70*i, 100, 1, i+1, i*.02, 1));
+    }
 }
 
 function draw() {
-  background(255);
-  strokeWeight(2);
-  grid(true);
- // myFunction();
+    background(200);
+    fill(0);
 
-  if (potential == 3) {
-    strokeWeight(2);
-    stroke(0, 250, 0);
-    line(0, width/2+height/2, width/2+height/2, 0);
-  }
-
-  if (potential == 4) {
-    fill(0, 255, 0);
-    ellipse(width/2, height/2, 12, 12);
-  }
-
-  if (potential == 5) {
-    strokeWeight(2);
-    stroke(120);  
-    for (var i = 0; i<height; i=i+10) {
-      line(width/2, i, width/2, i+5);
+    for (var i = 0; i < trainLength; i++ ) { 
+        redtrains[i].display();
+        bluetrains[i].display();
     }
-  }
 
-  if (keyIsPressed===true) {
-    optimizer();
-  }
 
-  nodeNumSlider.mouseReleased(numCheck);
+    if (metcounter > 0) {
+        m.display();
+        n.display();
+    }
   
-  for (var i = 0; i<nodes.length; i++) {
-    nodes[i].clickedOn();
-    nodes[i].display();
-    if (i>0) {
+
+
+   if (leftYes === true) {
+        leftPhoto = new Meteor(color(0), 100, 310, 0);
+        leftPhoto.display();
+    }
+
+    if (rightYes === true) {
+        rightPhoto = new Meteor(color(0), 250, 310, 0);
+        rightPhoto.display();
+    }
+    
+    //Make a left photo if there's at least one leftclick
+    if (counter > 0) {
+        noFill();
+        strokeWeight(1);
         stroke(0);
-        strokeWeight(2);
-        line(nodes[i].x, nodes[i].y, nodes[i-1].x, nodes[i-1].y);
+        rect(104, 310, 100, 140);
+        leftRed = new Train(color(250, 120, 120), 100, 290, 0, leftRedSerial, leftRedClock, -1);
+        leftBlue = new Train(color(120, 120, 250), 108, 330, 0, leftBlueSerial, leftBlueClock, 1);
+        leftRed.display();
+        leftBlue.display();
     }
-  }
-}
 
-function grid(y) {
-  if (y == true) {
-    strokeWeight(0.5);
-    stroke(150);
-    for (var i = 1; i<9*2; i++) {
-      line(width, .5*i*height/9, 0, .5*i*height/9);
+    if (counter > 1) {
+        noFill();
+        stroke(1);
+        rect(254, 310, 100, 140);
+        rightRed = new Train(color(250, 120, 120), 250, 290, 0, rightRedSerial, rightRedClock, -1);
+        rightBlue = new Train(color(120, 120, 250), 258, 330, 0, rightBlueSerial, rightBlueClock, 1);
+        rightRed.display();
+        rightBlue.display();
     }
-    for (var i = 1; i<9*4; i++) {
-      line(.5*i*height/9, 0, .5*i*height/9, height);
-    }
-  }
-}
-
-function Node(ix, iy) {
-  this.x = ix;
-  this.y = iy;
-  this.selected = false;
-
-  this.display = function() {
-    noStroke();
-    if (this.selected == false) {
-      fill(0);
-    }
-    else {
-      fill(255, 0, 0);
-    }
-    ellipse(this.x, this.y, 8, 8);
-  }
-
- this.clickedOn = function() {
-    if (this.selected == true) {
-      this.x = mouseX;
-      this.y = mouseY;
-    }
-  }
-}
-
-function calculateK() {
-  var K = 0;
-  for (var i=0; i<nodes.length; i++) {  
-    if (i>0) {
-      K = K + sq(dist(nodes[i].x, nodes[i].y, nodes[i-1].x, nodes[i-1].y));
-    }
-  }
-  return K;
-}
-
-function calculateU() {
-  U = 0;
-  for (var i=0; i<nodes.length; i++) {  
-    U = U + getPE(nodes[i]);
-  }
-  return U;
 }
 
 
-function getPE(q) {
-  switch(potential) {
-  case 1:
-    return -(3000/(nodeNum*nodeNum)*(q.y) -3800000/(nodeNum*nodeNum)  );
-  case 2:
-    return -(3000/(nodeNum*nodeNum)*(q.x) - 1900000/(nodeNum*nodeNum) );
-  case 3:
-    if (q.x + q.y > width/2 + height/2 ) {
-      return -(30000);
-    }
-    else {
-      return 0;
-    }
-  case 4:
-    return -(300000)*(1/(dist(q.x, q.y, width/2, height/2)));
-  case 5:
-    return (10.0/(nodeNum*nodeNum))*(sq(q.x-width/2));
-  default:
-  return 0;
-  }
-}
+function keyPressed() {
+    if (keyCode == UP_ARROW) {
+        for (var i = 0; i < trainLength; i ++ ) {
+            redtrains[i].forward();
+            bluetrains[i].forward();
+            if (metcounter > 0) {
+                m.forward();
+                n.forward();
+            }
+            timer = timer - 0.000061;
+            redtrains[i].clockUpdateForward();
+            bluetrains[i].clockUpdateForward();
+        }
+    } 
 
-function numCheck(){
-if (nodeNum != nodeNumSlider.value()){
- nodes.length = 0;
-    nodeNum = nodeNumSlider.value();
-   for (var i = 0; i<nodeNum; i++) {
-    nodes.push(new Node(map(i, 0, nodeNum-1, 30, width-80), height/2));
-  }
-   initialU = calculateU();
-   initialK = calculateK();
-   initialAction = calculateK() - calculateU();  
-   most = max(initialAction, initialU, abs(initialK)); 
+    else if (keyCode == DOWN_ARROW) {
+        for (var i = 0; i < trainLength; i ++ ) {
+            redtrains[i].backward();
+            bluetrains[i].backward();
+            if (metcounter > 0) {
+                m.backward();
+                n.backward();
+            }
+        timer = timer + 0.000061;
+        redtrains[i].clockUpdateBackward();
+        bluetrains[i].clockUpdateBackward();
+        }
+    }
+
+    else if (key == " ") {
+      m = new Meteor(0, redtrains[4].xpos, 80, -0.142);
+      n = new Meteor(0, redtrains[4].xpos, 80, 0.142);  // .14 = light speed
+      metcounter++;
   }
 }
 
 function mouseClicked() {
-  if (mouseButton == LEFT) {
-    for (var i = 0; i < nodes.length; i++) {
-      var p = nodes[i];
-      if (dist(mouseX, mouseY, p.x, p.y) < 5) {
-        nodes[i].selected =  !nodes[i].selected;
-        for (var j = 0; j < nodes.length; j++) {
-          if (j != i) {
-            nodes[j].selected = false;
+  if (mouseButton == LEFT) {  
+    //go through each tran. If any red are clicked on, store their info 
+    //if there is a photo already, pass it right
+      for (var i = 0; i < trainLength; i ++ ) {
+        if (redtrains[i].clickedOn()) { 
+            if (counter > 0) {
+                rightRedSerial = leftRedSerial;
+                rightRedClock = leftRedClock;
+                rightBlueSerial = leftBlueSerial;
+                rightBlueClock = leftBlueClock;
+                rightYes = leftYes;
+            }
+          leftRedSerial = redtrains[i].serial;
+          leftRedClock = redtrains[i].clock;
+          counter++;
+
+        //check if a blue rocket or meteor is across
+            for (var j = 0; j < bluetrains.length; j ++ ) { 
+                if (bluetrains[j].isAcrossFrom(redtrains[i])) {
+                    leftBlueSerial = bluetrains[j].serial;
+                    leftBlueClock = bluetrains[j].clock;
+                    break;
+                }
+                else {
+                    leftBlueSerial = 20;
+                }
+            }
+
+            if (metcounter > 0) {
+                if (redtrains[i].meteorCheck(m)== true || redtrains[i].meteorCheck(n) ==true) {
+                    leftYes = true;
+                }
+                else {
+                    leftYes = false;
+                }
+            }
+        
+        break;
+        }
+
+    }
+
+    for (var i = 0; i < trainLength; i ++ ) {
+      //Check if a blue train is clicked on
+      if (bluetrains[i].clickedOn()) {
+          if (counter > 0) {
+              rightRedSerial = leftRedSerial;
+              rightRedClock = leftRedClock;
+              rightBlueSerial = leftBlueSerial;
+              rightBlueClock = leftBlueClock;
+              rightYes = leftYes;
+          }
+        leftBlueSerial = bluetrains[i].serial;
+        leftBlueClock = bluetrains[i].clock;
+        counter++;
+
+
+        //check if a red rocket or meteor is across
+        for (var j = 0; j < trainLength; j ++ ) {
+          if (redtrains[j].isAcrossFrom(bluetrains[i])) {
+            leftRedSerial = redtrains[j].serial;
+            leftRedClock = redtrains[j].clock;
+            break;
+          }
+
+          else {
+            leftRedSerial = 20;
           }
         }
+
+        if (metcounter > 0) {
+          if (bluetrains[i].meteorCheck(m)==true || bluetrains[i].meteorCheck(n)==true) {
+            leftYes = true;
+          }
+          else {
+            leftYes = false;
+          }
+        }
+        
       }
     }
   }
 
-  if (mouseButton == RIGHT) {
-    nodes.length = 0;
-    nodeNum = nodeNumSlider.value();
-   for (var i = 0; i<nodeNum; i++) {
-    nodes.push(new Node(map(i, 0, nodeNum-1, 30, width-80), height/2));
-  }
-   initialU = calculateU();
-   initialK = calculateK();
-   initialAction = calculateK() - calculateU();  
-   most = max(initialAction, initialU, abs(initialK));  
-  }
+ 
 }
 
-function optimizer() {
-  var jump = 1;
-  if (potential ==3){
-    jump = 20;
-  }
-  for (var j = 0; j<250; j++) {
-    for (var i = 1;  i<nodeNumSlider.value()-1; i++) {
-      var oldAction = calculateK() - calculateU();
-      var tempX = nodes[i].x;
-      var tempY = nodes[i].y;
-      nodes[i].x = nodes[i].x + randomGaussian()*jump;
-      nodes[i].y = nodes[i].y + randomGaussian()*jump;
-      if ( calculateK() - calculateU() > oldAction) {
-        nodes[i].x  = tempX;
-        nodes[i].y = tempY;
-      }
+function Train(c_, xpos_, ypos_, xspeed_, serial_, offset_, direction_) {
+    this.c = c_;
+    this.xpos = xpos_;
+    this.ypos = ypos_;
+    this.xspeed = xspeed_;
+    this.serial = serial_;
+    this.clock = offset_;
+    this.stored = 0;
+    this.direction = direction_;
+
+
+    this.display = function() {
+        rectMode(CENTER);
+        noStroke();
+
+    //Draw a blank if serial = 20
+        if (this.serial == 20) {
+            fill(200); 
+            rect(this.xpos, this.ypos, 48, 24);
+            return 0;
+        }
+
+    //Draw rocket
+        else {
+            fill(this.c);
+            rect(this.xpos, this.ypos, 36, 20);
+            triangle(this.xpos- this.direction*25, this.ypos, this.xpos-18*this.direction, this.ypos+10, this.xpos-18*this.direction, this.ypos-10); 
+
+      //Write serial number
+            fill(0);
+            textAlign(CENTER);
+            text(this.serial.toString(), this.xpos + 5 -this.direction*3, this.ypos+5);
+
+      //Write clockreading
+            textAlign(CENTER);
+            text(String(this.clock.toFixed(3)), this.xpos + 1 - 2*this.direction, this.ypos+ 5+ 20*this.direction);
+            return this.clock;
+        }
     }
-  }  
+
+    this.forward = function() {
+        this.xpos = this.xpos + this.xspeed;
+    }
+
+    this.backward = function() {
+        this.xpos = this.xpos - this.xspeed;
+    }
+
+    this.clockUpdateForward = function() {
+        this.clock = this.clock - .000855;
+        return this.clock;
+    }
+
+    this.clockUpdateBackward = function() {
+        this.clock = this.clock + .000855;
+        return this.clock;
+    }
+
+    this.meteorCheck = function(q) {    //q is a meteor
+      
+        if (this.xpos > q.xpos + 4 && this.xpos < q.xpos+10 && this.direction ==1) {
+            return true;
+        }
+        else if (this.xpos > q.xpos - 1.5 && this.xpos < q.xpos+2.5 && this.direction ==-1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    this.clickedOn = function() {
+        if (mouseX < this.xpos + 10 && mouseX > this.xpos -10 && mouseY < this.ypos+30 && mouseY > this.ypos- 30) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    this.isAcrossFrom = function(s) {       //s is a train
+        if (this.xpos < s.xpos + 13 && this.xpos > s.xpos-10 && this.direction == 1 ) {
+            return true;
+        }
+        if (this.xpos < s.xpos && this.xpos > s.xpos -13 && this.direction == -1 ) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
 
+function Meteor(c_, xpos_, ypos_, xspeed_) {
+    this.c = c_;
+    this.xpos = xpos_;
+    this.ypos = ypos_;
+    this.xspeed = xspeed_;
+  
+    this.display = function() {
+        ellipse(this.xpos+4, this.ypos, 8, 8);
+        return this.xpos;
+    }
+    
+    this.forward = function() {
+        this.xpos = this.xpos + this.xspeed;
+    }   
 
-function myFunction() {
-    potential = document.getElementById("menu1").value;
-    return false;
-  }
+    this.backward = function() {
+        this.xpos = this.xpos - this.xspeed;
+    }
+}
